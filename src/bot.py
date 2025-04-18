b907458ec1b4e64da6268f7f636451e13a6f2324
Lyzix <maksim.v1liev@gmail.com>
2025-04-18 17:31:06 +0300

    lol

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,13 @@
 import os
 
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher, F, Router
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
 from dotenv import load_dotenv
 import logging
-from src.database import add_user
+import src.database
 from aiogram.filters import Command
-
 
 load_dotenv()
 bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -15,10 +16,29 @@ logging.basicConfig(level=logging.INFO)
 dp = Dispatcher()
 bot = Bot(token=bot_token)
 
+router = Router()
+
+
+# Состояния для регистрации
+class RegistrationStates(StatesGroup):
+    almost_reg = State()
+
 
 @dp.message(Command('start'))
-async def start(message: Message) -> None:
-    await message.reply("ПРИВЕТ!")
+async def start(message: Message, state: FSMContext) -> None:
+    if src.database.has_user(message.from_user.id):
+        await message.reply("ПРИВЕТ!")
+    else:
+        await message.reply("ВВЕДИТЕ СВОЕ ИМЯ И ФАМИЛИЮ ДЛЯ ЗАВЕРШЕНИЯ РЕГИСТРАЦИИ")
+        await state.set_state(RegistrationStates.almost_reg)
+
+
+@router.message(RegistrationStates.almost_reg, F.text)
+async def handle_register(message: Message) -> None:
+    await message.reply('ты гей')
+
+
+dp.include_router(router)
 
 
 async def main() -> None:
--- src/database.py
+++ src/database.py
@@ -9,4 +9,9 @@ def add_user(user_id: int, name: str):
                    (user_id, name,))
     connection.commit()
 
-def has_user(user_id)
+
+def has_user(user_id):
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM users WHERE user_id=?',
+                   (user_id,))
+    return cursor.fetchone()
