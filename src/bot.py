7e8c5d26591e670f819ab7c1d4d04f58957aae54
Lyzix <maksim.v1liev@gmail.com>
2025-04-18 18:20:02 +0300

    lol

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,5 @@
 import os
-
+import requests
 from aiogram import Bot, Dispatcher, F, Router
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
@@ -22,21 +22,42 @@ router = Router()
 # Состояния для регистрации
 class RegistrationStates(StatesGroup):
     almost_reg = State()
+    reg = State()
 
 
 @dp.message(Command('start'))
 async def start(message: Message, state: FSMContext) -> None:
-    if src.database.has_user(message.from_user.id):
+    if src.database.user_data(message.from_user.id):
         await message.reply("ПРИВЕТ!")
+        await state.set_state(RegistrationStates.reg)
     else:
         await message.reply("ВВЕДИТЕ СВОЕ ИМЯ И ФАМИЛИЮ ДЛЯ ЗАВЕРШЕНИЯ РЕГИСТРАЦИИ")
         await state.set_state(RegistrationStates.almost_reg)
 
 
 @router.message(RegistrationStates.almost_reg, F.text)
-async def handle_register(message: Message) -> None:
-    await message.reply('ты гей')
+async def handle_register(message: Message, state: FSMContext) -> None:
+    data = requests.get('http://127.0.0.1:5000/users').json()
+    ans = False
+    for x in data:
+        if message.text.lower() == x['name'].lower():
+            ans = True
+            break
 
+    if ans:
+        await message.reply("ТЫ МОЛОДЕЦ ПОЛЬЗУЙСЯ БОТОМ")
+        src.database.add_user(message.from_user.id, message.text.lower())
+        await state.set_state(RegistrationStates.reg)
+        return
+
+    await message.reply("НЕТ ТЫ ВВЕЛ НЕ ТО ИМЯ ПРОБУЙ ЕЩЕ РАЗ")
+
+
+@router.message(RegistrationStates.reg, F.text)
+async def send_data(message: Message, state: FSMContext) -> None:
+    user_name = src.database.user_data(message.from_user.id)[1]
+    data = requests.get(f'http://127.0.0.1:5000/users/{user_name}').json()
+    print(data.scores)
 
 dp.include_router(router)
 
--- src/database.py
+++ src/database.py
@@ -1,17 +1,43 @@
 import sqlite3
+import os
 
-connection = sqlite3.connect('user_db.db')
+DB_FILE = 'user_db.db'
+
+
+def init_db():
+    db_exists = os.path.exists(DB_FILE)
+
+    con = sqlite3.connect(DB_FILE)
+    cursor = con.cursor()
+
+    if not db_exists:
+        print(f"Создаем новую базу данных в файле {DB_FILE}")
+        cursor.execute('''
+        CREATE TABLE users (
+            user_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL
+        )
+        ''')
+        con.commit()
+        print("Таблица 'users' успешно создана")
+    else:
+        print(f"База данных {DB_FILE} уже существует")
+
+
+init_db()
+connection = sqlite3.connect(DB_FILE)
 
 
 def add_user(user_id: int, name: str):
     cursor = connection.cursor()
     cursor.execute('INSERT INTO users(user_id, name) VALUES (?, ?)',
-                   (user_id, name,))
+                   (user_id, name))
     connection.commit()
 
 
-def has_user(user_id):
+def user_data(user_id):
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM users WHERE user_id=?',
                    (user_id,))
     return cursor.fetchone()
+
